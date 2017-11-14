3f1c078df49da3c3365c683f96bf56f3e2efcc26
Javier Jimenez and Michael Brooks <javier@>
2017-11-14 10:47:56 -0800

    add routing and errors for edit, new, show

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -3,4 +3,44 @@ class WeeksController < ApplicationController
     @weeks = Week.all
     render :index
   end
+
+  def show
+    @week = Week.find(params[:id])
+      render :show
+  end
+
+  def new
+    @week = Week.new
+    render :new
+  end
+
+  def create
+    @week = Week.new(week_params)
+    if @week.save
+      redirect_to weeks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @week = Week.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @week = Week.find(params[:id])
+    if @week.update(week_params)
+      redirect_to weeks_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+    def week_params
+      params.require(:week).permit(:name)
+    end
+
 end
